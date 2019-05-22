081416ed0da84de50d810597f5530ddc14eaffda
fabriziofranco <fabrizio.franco@utec.edu.peUt3c2675clearREADME.md it config --global user.email fabrizio.franco@utec.edu.pe>
2019-05-22 14:13:15 -0500

    22-05-2019

--- web/server.py
+++ web/server.py
@@ -12,14 +12,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-@app.route('/books')
-def books():
-    db_session = db.getSession(engine)
-    users = db_session.query(entities.Book)
-    data = users[:]
-    return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype = 'application/json')
-
 @app.route('/users')
 def users():
     db_session = db.getSession(engine)
@@ -28,22 +20,27 @@ def users():
     return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype = 'application/json')
 
 
-@app.route('/create_test_books', methods = ['GET'])
-def create_test_books():
+@app.route('/create_user', methods = ['GET'])
+def create_user():
     db_session = db.getSession(engine)
-    book = entities.Book(name="Head First HTML5", isbn="12345", title="Head first about HTML5")
-    db_session.add(book)
-    db_session.commit()
-    return "Test books created!"
-
-
-@app.route('/create_test_users', methods = ['GET'])
-def create_test_users():
-    db_session = db.getSession(engine)
-    user = entities.User(name="David", apellido="Lazo", codigo="1234", password="qwerty")
+    user = entities.User(name="David", fullname="Lazo", username="1234", password="qwerty")
     db_session.add(user)
     db_session.commit()
     return "Test user created!"
+
+@app.route('/users/<id>', methods = ['GET'])
+def get_user(id):
+    db_session = db.getSession(engine)
+    users = db_session.query(entities.User).filter(entities.User.id==id)
+    for user in users:
+        js=json.dumps(user,cls=connector.AlchemyEncoder)
+        return Response(js,status=200, mimetype='application/json')
+
+    message={'status':404,'message':'Not found'}
+    return Response(message, status=404,mimetype='application/json')
+
+
+
 
 if __name__ == '__main__':
     app.secret_key = ".."
